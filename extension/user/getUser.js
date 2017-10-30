0ddc534cdae1208ed7e27e2ec6c3ee191d3fff2a
Steffen Zull <steffen.zull@shopgate.com>
2017-10-30 15:16:21 +0100

    Fix mock user

--- extension/user/getUser.js
+++ extension/user/getUser.js
@@ -7,9 +7,9 @@ module.exports = function (context, input, cb) {
   if (context.meta.userId) {
     return cb(null, {
       id: '87ddbbdb-5b09-4dc4-b1b0-214f4b0fdaa1',
-      mail: 'steffen.zull@shopgate.com',
-      firstName: 'Steffen',
-      lastName: 'Zull'
+      mail: '',
+      firstName: '',
+      lastName: ''
     })
   }
 
