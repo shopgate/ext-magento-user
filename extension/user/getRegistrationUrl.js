27a58c5af6117ed08f24e64e9f20ecdd46eb56dc
Ronny Lindner <ronny.lindner@shopgate.com>
2017-12-13 11:40:29 +0100

    WEBC-496 - Refactoring and adding UTM-Parameters to Registration-URL

--- extension/user/getRegistrationUrl.js
+++ extension/user/getRegistrationUrl.js
@@ -1,8 +1,19 @@
+const UtmParameters = require ('../models/utmParameters/utmParameters')
+
 /**
+ * @typedef {object} context.config
+ * @property {string} registrationUrl
+ *
  * @param {object} context
  * @param {object} input
  * @param {function} cb
  */
 module.exports = function (context, input, cb) {
-  cb(null, {url: context.config.registrationUrl})
+
+  const RegistrationUrlUtmParameters = new UtmParameters()
+  RegistrationUrlUtmParameters.source = 'shopgate'
+  RegistrationUrlUtmParameters.medium = 'app'
+  RegistrationUrlUtmParameters.campaign = 'web-register'
+
+  cb(null, {url: context.config.registrationUrl + RegistrationUrlUtmParameters.getQueryParameters()})
 }
