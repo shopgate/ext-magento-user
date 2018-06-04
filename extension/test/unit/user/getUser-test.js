8bd6facad047c6c2de0e956cf50dd5a15067965d
Andre Kraus <andre.kraus@shopgate.com>
2018-06-04 09:36:15 +0200

    WEBC-207: now also returning address data, if it's returned by the plugin

--- extension/test/unit/user/getUser-test.js
+++ extension/test/unit/user/getUser-test.js
@@ -2,7 +2,7 @@ const nock = require('nock')
 const assert = require('assert')
 const describe = require('mocha').describe
 const it = require('mocha').it
-const before = require('mocha').before
+const beforeEach = require('mocha').beforeEach
 
 const getUser = require('../../../user/getUser')
 
@@ -29,10 +29,39 @@ describe('getUser', () => {
     'customer_id': 123,
     'email': 'john.doe@shopgate.com',
     'firstname': 'John',
-    'lastname': 'Doe'
+    'lastname': 'Doe',
+    'dob': '2018-06-06 00:00:00',
+    'gender': '1',
+    'addresses': [{
+      'customer_address_id': '113',
+      'created_at': '2018-06-01T02:57:49-07:00',
+      'updated_at': '2018-06-01 09:57:49',
+      'increment_id': null,
+      'city': 'Magdeburg',
+      'company': 'Shopgate GmbH',
+      'country_id': 'DE',
+      'fax': null,
+      'firstname': 'André',
+      'lastname': 'Kraus',
+      'middlename': null,
+      'postcode': '39122',
+      'prefix': null,
+      'region': 'Sachsen-Anhalt',
+      'region_id': '92',
+      'street': 'Olvenstedter Straße 11a',
+      'suffix': null,
+      'telephone': '01622355954',
+      'vat_id': null,
+      'vat_is_valid': null,
+      'vat_request_date': null,
+      'vat_request_id': null,
+      'vat_request_success': null,
+      'is_default_billing': true,
+      'is_default_shipping': false
+    }]
   }
 
-  before(() => {
+  beforeEach(() => {
     nock('http://magento.shopgate.com/shopgate/v2')
       .get('/customers/me')
       .reply(200, magentoResponse)
@@ -44,6 +73,35 @@ describe('getUser', () => {
     assert.equal(magentoResponse.customer_id, userData.id)
     assert.equal(magentoResponse.firstname, userData.firstName)
     assert.equal(magentoResponse.lastname, userData.lastName)
+    assert.equal('2018-06-06', userData.birthday)
+    assert.equal('m', userData.gender)
+
+    const magentoAddress = magentoResponse.addresses.pop()
+    const userAddress = userData.addresses.pop()
+
+    assert.equal(magentoAddress.customer_address_id, userAddress.id)
+    assert.equal('invoice', userAddress.type)
+    assert.equal(magentoAddress.firstname, userAddress.firstName)
+    assert.equal(magentoAddress.lastname, userAddress.lastName)
+    assert.equal(magentoAddress.company, userAddress.company)
+    assert.equal(magentoAddress.street, userAddress.street1)
+    assert.equal(magentoAddress.city, userAddress.city)
+    assert.equal(magentoAddress.telephone, userAddress.phone)
+    assert.equal(1, userAddress.isDefault)
+    assert.equal(magentoAddress.postcode, userAddress.zipcode)
+    assert.equal(magentoAddress.country_id, userAddress.country)
+  })
+
+  it('should return valid user data, even if now addresses are exported', async () => {
+    delete context.addresses
+    const userData = await getUser(context, input)
+    assert.equal(magentoResponse.email, userData.mail)
+    assert.equal(magentoResponse.customer_id, userData.id)
+    assert.equal(magentoResponse.firstname, userData.firstName)
+    assert.equal(magentoResponse.lastname, userData.lastName)
+    assert.equal('2018-06-06', userData.birthday)
+    assert.equal('m', userData.gender)
+    assert.deepEqual([], userData.addresses)
   })
 
   it('should return unauthorized error because of missing context.meta', async () => {
--- extension/user/getUser.js
+++ extension/user/getUser.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 const UnauthorizedError = require('../models/Errors/UnauthorizedError')
 const MagentoRequest = require('../lib/MagentoRequest')
 
@@ -12,11 +14,36 @@ module.exports = async (context, input) => {
 
   const endpointUrl = `${context.config.magentoUrl}/customers/me`
   const magentoResponse = await MagentoRequest.send(endpointUrl, context, input.token)
+  let addresses = []
+
+  // will be only returned with cloudapi plugin >= 3.1.4
+  if (magentoResponse.addresses) {
+    magentoResponse.addresses.forEach((address) => {
+      addresses.push({
+        id: address.customer_address_id,
+        type: address.is_default_billing === true ? 'invoice' : 'shipping',
+        firstName: address.firstname,
+        lastName: address.lastname,
+        company: address.company,
+        street1: address.street,
+        street2: null,
+        city: address.city,
+        phone: address.telephone,
+        isDefault: 1,
+        alias: null,
+        zipcode: address.postcode,
+        country: address.country_id
+      })
+    })
+  }
 
   return {
     id: magentoResponse.customer_id,
+    gender: magentoResponse.gender ? magentoResponse.gender === '1' ? 'm' : 'f' : '',
     mail: magentoResponse.email,
     firstName: magentoResponse.firstname,
-    lastName: magentoResponse.lastname
+    lastName: magentoResponse.lastname,
+    birthday: magentoResponse.dob ? moment(magentoResponse.dob, 'YYYY-MM-DD HH:mm').format('YYYY-MM-DD') : '',
+    addresses: addresses
   }
 }
