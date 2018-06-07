6c16a30aef98334196db0cbebf5cb28db5e58269
rutkowski <waldemar.rutkowski@shopgate.com>
2018-06-07 20:42:24 +0200

    PWA-484: Removed a redundant config param, that was passed to, but not used by MagentoRequest.send method.

--- extension/user/getUser.js
+++ extension/user/getUser.js
@@ -13,7 +13,7 @@ module.exports = async (context, input) => {
   }
 
   const endpointUrl = `${context.config.magentoUrl}/customers/me`
-  const magentoResponse = await MagentoRequest.send(endpointUrl, context, input.token, !context.config.allowSelfSignedCertificate)
+  const magentoResponse = await MagentoRequest.send(endpointUrl, context, input.token)
   let addresses = []
 
   // will be only returned with cloudapi plugin >= 3.1.4
