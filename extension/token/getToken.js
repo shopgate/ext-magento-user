7ad2f9306b534a04b696dc85ba60629ef7f04825
rutkowski <waldemar.rutkowski@shopgate.com>
2018-06-07 14:39:24 +0200

    PWA-484: Added a config entry to allow custom ssl certificates when accessing Magento REST API

--- extension/token/getToken.js
+++ extension/token/getToken.js
@@ -15,7 +15,7 @@ module.exports = function (context, input, cb) {
 
   const isLoggedIn = !!context.meta.userId
 
-  const th = new TokenHandler(clientCredentials, authUrl, storages, log, request)
+  const th = new TokenHandler(clientCredentials, authUrl, storages, log, request, context.config.validateSSLCertificate)
 
   th.getToken(isLoggedIn, (err, token) => {
     if (err) return cb(err)
