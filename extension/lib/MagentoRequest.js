81fb683806985196c4b570dae64240e3299c2c23
Ronny Lindner <ronny.lindner@shopgate.com>
2018-06-27 10:27:34 +0200

    WEBC-700 - Improved logging, added status code and duration

--- extension/lib/MagentoRequest.js
+++ extension/lib/MagentoRequest.js
@@ -27,27 +27,37 @@ class MagentoRequest {
 
     const tracedRequest = context.tracedRequest('magento-user-extension:MagentoRequest', {log: true})
 
-    context.log.debug(`Made a magento request ${util.inspect(options)}`)
+    context.log.debug(`Magento request: ${util.inspect(options)}`)
+    const startResponse = new Date()
     return await new Promise((resolve, reject) => {
       tracedRequest(
         options,
-        (error, response, body) => {
+        (error, response) => {
           if (error) {
+            this.log(context, startResponse, `Magento response: ${util.inspect(response.body)}`, response.statusCode)
             reject(new Error(error))
           } else if (response.statusCode === 401 || response.statusCode === 403) {
+            this.log(context, startResponse, `UnauthorizedError: ${util.inspect(response.body)}`, response.statusCode)
             reject(new UnauthorizedError())
           } else if (response.statusCode === 404) {
+            this.log(context, startResponse, `MagentoEndpointNotFoundError: ${util.inspect(response.body)}`, response.statusCode)
             reject(new MagentoEndpointNotFoundError())
           } else if (response.statusCode === 405) {
+            this.log(context, startResponse, `MagentoEndpointNotAllowedError: ${util.inspect(response.body)}`, response.statusCode)
             reject(new MagentoEndpointNotAllowedError())
-          } else if (body.messages && body.messages.error) {
+          } else if (response.body.messages && response.body.messages.error) {
+            this.log(context, startResponse, `MagentoEndpointError: ${util.inspect(response.body)}`, response.statusCode)
             reject(new MagentoEndpointError())
           } else { // This else is currently important, cause there is a bug within the tracedRequest which will crash the app otherwise
-            context.log.debug(`Magento response ${util.inspect(body)}`)
-            resolve(body)
+            this.log(context, startResponse, `Magento response: ${util.inspect(response.body)}`, response.statusCode)
+            resolve(response.body)
           }
         })
     })
+  }
+
+  static log (context, startResponse, message, statusCode) {
+    context.log.debug({duration: new Date() - startResponse, statusCode}, message)
   }
 }
 
