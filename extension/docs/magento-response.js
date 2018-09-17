bf055c7fb5e0555a867d066e8351145471aff344
Alexander Wesselburg <info@myportal.de>
2018-09-17 10:29:40 +0200

    MAGENTO-2116 - PR1 - change mage response documentation

--- extension/docs/magento-response.js
+++ extension/docs/magento-response.js
@@ -3,12 +3,12 @@
  */
 /**
  * @typedef {Object} MagentoResponseUser
- * @property {string} customer_id - id if the customer
+ * @property {string} customer_id - id of the customer
  * @property {string} created_at - when the customer was created, e.g. "2018-02-28 03:59:05"
  * @property {string} updated_at - when the customer was updated last, e.g. "2018-02-28 03:59:05"
  * @property {string} store_id - id of the store
  * @property {string} website_id - id of the website
- * @property {string} confirmation - confirmation of customer
+ * @property {string} confirmation - whether customer confirmed their email
  * @property {string} created_in - store where the customer are created
  * @property {string} default_billing - default billing address id
  * @property {string} default_shipping - default shipping address id
@@ -21,12 +21,10 @@
  * @property {string} lastname - customers last name
  * @property {string} middlename - customers middle name
  * @property {string} prefix - customers prefix
- * @property {string} reward_update_notification - customer revice reward update notifications
- * @property {string} reward_warning_notification - customer revice reward warning notifications
  * @property {string} rp_token - customers rp_token
  * @property {string} rp_token_created_at - customers rp_token created at
- * @property {string} suffix - suffix of customer
- * @property {string} taxvat - taxvat of customer
+ * @property {string} suffix - customer's suffix
+ * @property {string} taxvat - customer's VAT id
  * @property {MagentoResponseUserCustomerGroup} customer_group
  */
 
