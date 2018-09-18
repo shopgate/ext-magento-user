99e142e0e1a230c7ab81ab82cd2c87147ae0332e
Konstantin Kiritsenko <k@componentk.com>
2018-09-18 12:20:23 +0200

    MAGENTO-2127 Fixing unrelated unit tests
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

--- extension/test/unit/user/getUser-test.js
+++ extension/test/unit/user/getUser-test.js
@@ -50,7 +50,7 @@ describe('getUser', () => {
     assert.equal(magentoResponse.customer_id, userData.id)
   })
 
-  it('should return valid user data with usergroup and custom attrinutes', async () => {
+  it('should return valid user data with user group and custom attributes', async () => {
     nock('http://magento.shopgate.com/shopgate/v2')
       .get('/customers/me')
       .reply(200, magentoResponse)
@@ -97,9 +97,9 @@ describe('getUser', () => {
       .reply(200, customResponse)
 
     const userData = await getUser(context, input)
-    assert.equal(magentoResponse.email, userData.mail)
-    assert.equal(magentoResponse.customer_id, userData.id)
-    assert.deepEqual(userData.customAttributes, undefined)
+    assert.equal(userData.mail, magentoResponse.email)
+    assert.equal(userData.id, magentoResponse.customer_id)
+    assert.deepEqual(userData.customAttributes, {})
   })
 
   it('should return unauthorized error because of missing context.meta', async () => {
