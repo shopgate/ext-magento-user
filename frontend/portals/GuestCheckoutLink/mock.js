334ad841b9f9a389ec4ffc36db768cecdb857574
Alexander Wesselburg <info@myportal.de>
2018-09-24 15:46:45 +0200

    MAGENTO-2047 - PR1 - Add more jest tests

---
+++ frontend/portals/GuestCheckoutLink/mock.js
@@ -0,0 +1,38 @@
+/**
+ * Mocked state with redirectLocation to checkout page.
+ * @type {Object}
+ */
+export const mockedStateFromCartView = {
+  history: {
+    pathname: '/',
+    redirectLocation: {
+      pathname: '/checkout'
+    }
+  }
+}
+
+/**
+ * Mocked state with redirectLocation to more page.
+ * @type {Object}
+ */
+export const mockedStateFromMyAccountView = {
+  history: {
+    pathname: '/',
+    redirectLocation: {
+      pathname: '/more'
+    }
+  }
+}
+
+/**
+ * Mocked state with redirectLocation to home page.
+ * @type {Object}
+ */
+export const mockedStateFromAnyOtherView = {
+  history: {
+    pathname: '/',
+    redirectLocation: {
+      pathname: '/'
+    }
+  }
+}
--- frontend/portals/GuestCheckoutLink/spec.jsx
+++ frontend/portals/GuestCheckoutLink/spec.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import I18n from '@shopgate/pwa-common/components/I18n';
-import Link from '@shopgate/pwa-common/components/Router/components/Link';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import styles from './style'
-import { CHECKOUT_GUEST_PATH } from './route';
+import mockRenderOptions from '@shopgate/pwa-common/helpers/mocks/mockRenderOptions';
+import { mockedStateFromCartView, mockedStateFromMyAccountView, mockedStateFromAnyOtherView } from './mock'
 
+const mockedStore = configureStore();
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<div />', () => {
-  it('should render with one link', () => {
-    const component = Enzyme.mount(
-      <div className={styles.container}>
-        <I18n.Text string="checkout.or" className={styles.or} />
-        <Link href={CHECKOUT_GUEST_PATH} className={styles.guestCheckout}>
-          <I18n.Text string="checkout.continue_as_guest" />.
-        </Link>
-      </div>
-    )
+beforeEach(() => {
+  jest.resetModules();
+});
+
+/**
+ * Creates component with provided store state.
+ * @param {Object} mockedState Mocked stage.
+ * @return {ReactWrapper}
+ */
+const createComponent = (mockedState) => {
+  const GuestCheckoutLink = require('./index').default;
+  return Enzyme.mount(
+    <Provider store={mockedStore(mockedState)}>
+      <GuestCheckoutLink />
+    </Provider>,
+    mockRenderOptions
+  );
+};
+
+describe('<GuestCheckoutLink />', () => {
+  it('should render with one link for login in checkout page', () => {
+    const component = createComponent(mockedStateFromCartView);
     expect(component).toMatchSnapshot();
+    expect(component.find('Link').exists()).toBe(true);
+  });
+
+  it('should render null in case of default login', () => {
+    const component = createComponent(mockedStateFromMyAccountView);
+    expect(component).toMatchSnapshot();
+    expect(component.find('Link').exists()).toBe(false);
+  });
+
+  it('should render null in case of all other', () => {
+    const component = createComponent(mockedStateFromAnyOtherView);
+    expect(component).toMatchSnapshot();
+    expect(component.find('Link').exists()).toBe(false);
   });
 });
