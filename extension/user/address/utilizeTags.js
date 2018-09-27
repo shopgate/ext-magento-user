69b7e1a559a7bde5eb283c7b2367aa964701cb56
Andre Kraus <andre.kraus@shopgate.com>
2018-09-27 14:45:26 +0200

    MAGENTO-2098: fixed updateAddress pipeline

--- extension/user/address/utilizeTags.js
+++ extension/user/address/utilizeTags.js
@@ -1,8 +1,8 @@
 module.exports = async (context, input) => {
   const tags = input.tags && {
-      ...(input.tags.includes('default_shipping') && { is_default_shipping: 1 }),
-      ...(input.tags.includes('default_billing') && { is_default_billing: 1 })
-    }
+    ...(input.tags.includes('default_shipping') && { is_default_shipping: 1 }),
+    ...(input.tags.includes('default_billing') && { is_default_billing: 1 })
+  }
 
   return { magentoAddress: { ...input.magentoAddress, ...tags || {} } }
 }
