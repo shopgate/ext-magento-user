571af738b10e93c33815d77c3e762f51df537cef
Andre Kraus <andre.kraus@shopgate.com>
2018-11-29 09:14:56 +0100

    MAGENTO-2053: fixed frontend linting issues

--- frontend/portals/GuestCheckoutLink/index.jsx
+++ frontend/portals/GuestCheckoutLink/index.jsx
@@ -5,7 +5,7 @@ import Link from '@shopgate/pwa-common/components/Router/components/Link';
 import { CHECKOUT_GUEST_PATH } from './route';
 import connect from './connector';
 import styles from './style';
-import config from './config'
+import config from './config';
 
 /**
  * Check is the guest checkout disabled
