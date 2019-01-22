09a612b954e5f55456976aff2a2fd2cb03358dd2
Andre Kraus <andre.kraus@shopgate.com>
2019-01-22 10:30:41 +0100

    MAGENTO-2259: facebook login for new customers

--- extension/user/auth-strategy/facebook.js
+++ extension/user/auth-strategy/facebook.js
@@ -1,4 +1,5 @@
 const TokenHandler = require('../../helpers/tokenHandler')
+const MagentoRequest = require('../../lib/MagentoRequest')
 const InvalidCredentialsError = require('../../models/Errors/InvalidCredentials')
 
 /**
@@ -13,6 +14,7 @@ module.exports = async (context, input) => {
     return {}
   }
 
+  let magentoTokenResponse = null
   const authUrl = context.config.magentoUrl + '/auth/token'
   const { parameters: { success, profile } } = input
   const request = context.tracedRequest('magento-user-extension:facebook-login', { log: true })
@@ -23,7 +25,16 @@ module.exports = async (context, input) => {
 
   const th = new TokenHandler(context.config.credentials, authUrl, context.storage, context.log, request, !context.config.allowSelfSignedCertificate)
 
-  const magentoTokenResponse = await login(th, profile, input.strategy)
+  try {
+    console.log('jo')
+    magentoTokenResponse = await login(th, profile, input.strategy)
+  } catch (e) {
+    console.log('jo2')
+    await register(await getAuthToken(th), profile)
+    magentoTokenResponse = await login(th, profile, input.strategy)
+  }
+  console.log('jo3')
+
   await new Promise((resolve, reject) => {
     th.deleteGuestTokens((err) => {
       if (err) return reject(err)
@@ -34,15 +45,48 @@ module.exports = async (context, input) => {
   return { userId: profile.email, magentoTokenResponse }
 
   /**
-   * @param {TokenHandler} tokenHandler
-   * @param {UserLoginInputParameters} userCredentials
-   * @param {string} strategy
+   * @param {string} token
+   * @param {Object} profile
+   * @returns {Promise.<void>}
    */
+  async function register (token, profile) {
+    const user = {
+      email: profile.email,
+      firstname: profile.first_name,
+      lastname: profile.last_name
+    }
+
+    const magentoRequest = new MagentoRequest(context, token)
+    return magentoRequest.post(`${context.config.magentoUrl}/customers`, user)
+  }
+
+  /**
+ * @param {TokenHandler} tokenHandler
+ * @param {UserLoginInputParameters} userCredentials
+ * @param {string} strategy
+ */
   async function login (tokenHandler, userCredentials, strategy) {
     return new Promise((resolve, reject) => {
       tokenHandler.login(userCredentials, strategy, (err, magentoTokenResponse) => {
-        if (err) return reject(err)
+        if (err) {
+          return reject(err)
+        }
         resolve(magentoTokenResponse)
+      })
+    })
+  }
+
+  /**
+ * @param {TokenHandler} tokenHandler
+ * @returns {string} token
+ */
+  async function getAuthToken (tokenHandler) {
+    return new Promise((resolve, reject) => {
+      tokenHandler.getToken(false, (err, token) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(token)
       })
     })
   }
