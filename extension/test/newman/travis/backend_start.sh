50ee1fcc65b4bb4b8bfb68f5b7f1b43461d75f9f
Rainer Skistims <rainer.skistims@shopgate.com>
2019-03-11 12:57:29 +0100

    MAGENTO-2215 run backend in nyc

--- extension/test/newman/travis/backend_start.sh
+++ extension/test/newman/travis/backend_start.sh
@@ -3,7 +3,7 @@
 cd ${CONNECT_DIR}
 rm -f connect.out
 
-sgconnect backend start > connect.out 2>&1 &
+${EXT_DIR}/extension/node_modules/.bin/nyc sgconnect backend start > connect.out 2>&1 &
 
 echo "=== Waiting for backend ready signal ==="
 chmod +x ${EXT_DIR}/${TRAVIS_DIR}/backend_wait.sh
