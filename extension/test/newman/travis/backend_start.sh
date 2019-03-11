b37ab565eed04fa1df8af25e565ae408aa3e6c42
Rainer Skistims <rainer.skistims@shopgate.com>
2019-03-11 14:14:14 +0100

    MAGENTO-2215 move nyc report

--- extension/test/newman/travis/backend_start.sh
+++ extension/test/newman/travis/backend_start.sh
@@ -9,4 +9,3 @@ echo "=== Waiting for backend ready signal ==="
 chmod +x ${EXT_DIR}/${TRAVIS_DIR}/backend_wait.sh
 timeout 60s ${EXT_DIR}/${TRAVIS_DIR}/backend_wait.sh
 
-${EXT_DIR}/extension/node_modules/.bin/nyc report --reporter=text-lcov | coveralls
